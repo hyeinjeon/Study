85cfb25189d8651ccf97626fb4d4039548ede7d7
HYEIN JEON <57359259+hyeinjeon@users.noreply.github.com>
2022-06-23 21:09:18 +0900

    Create Book.java

---
+++ Java2/hw/hw3/Assignment3_2/Book.java
@@ -0,0 +1,37 @@
+package Assignment3_2;
+
+public class Book {
+
+	public String title;
+	public int pages;
+	public String author;
+	
+	public Book() {
+		
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+	public String getAuthor() {
+		return author;
+	}
+	
+	public void setPages(int pages) {
+		this.pages = pages;
+	}
+	
+	public int getPages() {
+		return pages;
+	}
+
+}
