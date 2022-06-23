d80bcfc2723217f31aaa6fc69c987288ffb9c5d7
HYEIN JEON <57359259+hyeinjeon@users.noreply.github.com>
2022-06-23 21:08:23 +0900

    Create Book.java

---
+++ Java2/hw/hw3/Assignment3_1/Book.java
@@ -0,0 +1,22 @@
+package Assignment3_1;
+
+public class Book {
+	
+	String title, author;
+
+	public Book(String title, String author) {
+		// TODO Auto-generated constructor stub
+		this.title = title;
+		this.author = author;
+	}
+	
+	//getter 설정
+	public String getTitle() {
+		return title;
+	}
+	
+	public String getAuthor() {
+		return author;
+	}
+	
+}
